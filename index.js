8c5ff9d1fa2403c983ec3fd8b9eca4eb289bbeeb
Emanuel Bravi <108707452+magobcode@users.noreply.github.com>
2023-01-16 14:05:00 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+// Middelware
+import express from 'express';
+import router from './routes/index.js';
+import db from './config/db.js';
+
+
+const app = express();
+
+// conectar la base de datos
+db.authenticate()
+    .then( () => console.log('Base de datos conectada'))
+    .catch( error => console.log(error))
+
+// Definir puerto
+const port = process.env.PORT || 4000;
+
+// Habilitar PUG 
+app.set('view engine', 'pug');
+
+// obtener el año actual
+app.use( (req, res, next) => { 
+    const year = new Date(); 
+
+    res.locals.actualYear = year.getFullYear();
+    res.locals.nombreSitio = "Agencia de Viajes";
+    next();
+});
+
+// Agregar body parser para leer los datos del formulario
+app.use(express.urlencoded({extended: true}));
+
+// definir la carpeta publica
+app.use(express.static('public'));
+
+// agregar router
+app.use('/', router);
+
+app.listen(port, () => {  
+    console.log(`El Servidor esta funcionando en el puerto ${port}`)
+})
